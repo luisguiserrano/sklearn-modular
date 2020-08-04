73b5e42f79e916156b9d5005ccdeb169f753c97a
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-04 19:20:43 -0400

    Adding training

--- src/python/modular/functions.py
+++ src/python/modular/functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn
 
 def preprocess_data(data:dict) -> dict:
     df = pd.DataFrame.from_dict(data)['data']
@@ -26,3 +27,16 @@ def preprocess_data(data:dict) -> dict:
     result["labels"] = labels
 
     return result
+
+def train_model(features, labels):
+    model = sklearn.linear_model.LogisticRegression()
+    model.fit(features, labels)
+    return model
+
+def make_predictions(model, features):
+    predictions = model.predict(features)
+    return predictions
+
+def score_model(model, features, labels):
+    score = model.score(features, labels)
+    return score
