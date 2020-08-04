0126ee3f6dd1db74a9313522cd3d588b69e7c76b
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-04 18:21:24 -0400

    Fix

--- src/python/modular/functions.py
+++ src/python/modular/functions.py
@@ -2,17 +2,23 @@ import pandas as pd
 
 def preprocess_data(data:dict) -> dict:
     df = pd.DataFrame.from_dict(data)
-    
+
+    print("DF")
+    print(df)
+
+    print("KEYS")
+    print(df.keys())
+
     features = df[df.keys()[1:-1]]
     labels = df[df.keys()[-1]]
-
-    result = {}
-    result["features"] = features.to_dict()
-    result["labels"] = labels.to_dict()
 
     print("FEATURES")
     print(features)
     print("LABELS")
     print(labels)
+
+    result = {}
+    result["features"] = features.to_dict()
+    result["labels"] = labels.to_dict()
 
     return result
