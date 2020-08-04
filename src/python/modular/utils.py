adedcd64ffa1d3fbd822d610229b6b04e3cdb266
luisguiserrano <luisgui.serrano@gmail.com>
2020-08-04 15:45:00 -0400

    Adding save_json

--- src/python/modular/utils.py
+++ src/python/modular/utils.py
@@ -22,3 +22,30 @@ def read_dataset(filename):
 
     except Exception as e:
         print(f'Errors: could not load dataset: {e}')
+
+def save_json(result, filename) -> None:
+    """
+    Saves data as JSON.
+    Args:
+        result (ditc): of data to save.
+        filenames (str): file name to save the data in
+            (should have a '.json' extension).
+    """
+    try:
+        with open(filename,'w') as f:
+            result["schema"] = "orquestra-v1-data"
+            f.write(json.dumps(result, indent=2)) 
+
+    except IOError:
+        print(f'Error: Could not open {filename}')
+
+class NumpyArrayEncoder(JSONEncoder):
+    """
+    Aux classes for decoding NumPy arrays to Python objects.
+    Returns:
+        A list or a JSONEnconder object.
+    """
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
